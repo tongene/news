920909db7ff3488fd9a1d65fd4539c95c107cec9
tongene <ngene.christina@gmail.com>
2025-03-15 20:10:49 -0100

    news-repo-15/03-25

--- components/ConfirmModal.tsx
+++ components/ConfirmModal.tsx
@@ -27,7 +27,7 @@ const ConfirmModal = () => {
      {prX?.includes('logout')? 
      <div className="p-8 bg-gray-800 bg-opacity-40 m-auto z-50 h-full -top-1 fixed w-full left-0 text-center flex items-center justify-center"> 
      <div className="bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl place-self-center ">
-     <p className="text-2xl p-4 font-bold opacity-70">Do you want to logout?</p>   
+     <p className="text-2xl p-4 font-bold opacity-70 text-black">Do you want to logout?</p>   
     <button onClick={()=> router.back()}className="cursor-pointer hover:bg-opacity-70 m-1 text-xl bg-gray-900 px-3 py-2 w-1/2 text-white">Return</button>  
   <form className="m-1 flex m-auto justify-center"action={signOutAction} >  
  <button type="submit" className="cursor-pointer text-xl px-3 text-center py-2 w-1/2 text-red-400 m-1 hover:bg-gray-100">Yes</button > 
