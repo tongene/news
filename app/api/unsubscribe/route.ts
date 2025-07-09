7cb4af7cd9131281fcdb6e6dd157866b31901b61
culturays <culturaysnet@gmail.com>
2025-07-09 22:50:53 -0100

    deploy-1.5-/9/07

--- app/api/unsubscribe/route.ts
+++ app/api/unsubscribe/route.ts
@@ -1,10 +1,9 @@
-import { NextApiRequest, NextApiResponse } from 'next';
 import { createClient } from '@supabase/supabase-js';
 import { NextRequest, NextResponse } from 'next/server';
 
 const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE_SECRET!);
 
-export async function POST(req: NextRequest, res: NextApiResponse) {
+export async function POST(req: NextRequest, res:any) {
   const resp=await req.json()
   if (!resp.email || typeof resp.email !== 'string') {
     return NextResponse.json({ message: 'Invalid email.' },
@@ -20,16 +19,14 @@ export async function POST(req: NextRequest, res: NextApiResponse) {
     return NextResponse.json({ message: 'Failed to unsubscribe.' },
       { status: 500 });
   }
- return NextResponse.json(
-      { message: `
+ return res.send( `
     <html>
       <body style="font-family: Arial; text-align: center; padding: 40px;">
         <h2>You have been unsubscribed.</h2>
         <p>You’ll no longer receive emails from GoWork Africa Reinvented.</p>
       </body>
     </html>
-  `  },
-      { status: 200 }
+  ` 
     );
  
 }
