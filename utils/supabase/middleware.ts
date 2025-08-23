5d20df5b15475bcf6a030e65a6e8127e5972d68d
tongene <ngene.christina@gmail.com>
2025-08-22 23:02:26 -0100

    deploy-1.2/21/08

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -4,55 +4,93 @@ import { type NextRequest, NextResponse } from "next/server";
 export const updateSession = async (request: NextRequest) => {
  // try {
     // Create an unmodified response
-    let response = NextResponse.next({
-      request: {
-        headers: request.headers,
-      },
-    });
 
-    const supabase = createServerClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-      {
-        cookies: {
-          getAll() {
-            return request.cookies.getAll();
-          },
-          setAll(cookiesToSet) {
-            cookiesToSet.forEach(({ name, value }) =>
-              request.cookies.set(name, value),
-            );
-            // response = NextResponse.next({
-            //   request,
-            // });
-            cookiesToSet.forEach(({ name, value, options }) =>
-              response.cookies.set(name, value, options),
-            );
-          },
-        },
+  if (request.nextUrl.pathname.startsWith("/auth/callback")) {
+    return NextResponse.next();
+  }
+
+  const response = NextResponse.next();
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll: () => request.cookies.getAll(),
+        setAll: (cookies) =>
+          cookies.forEach(({ name, value, options }) => {
+            response.cookies.set(name, value, options);
+          }),
       },
-    );
+    }
+  );
+
+  const { data: { user } } = await supabase.auth.getUser();
+  const path = request.nextUrl.pathname;
+
+  const unprotectedPaths = ["/sign-in/", "/forgot-password/"];
+
+  if (path.startsWith("/protected/") && !user) {
+    return NextResponse.redirect(new URL("/sign-in/", request.url));
+  }
+
+  if (user && unprotectedPaths.some((up) => path.startsWith(up))) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+ response.headers.set('confirm',request.nextUrl.searchParams.toString() ) 
+ response.headers.set('x-url',request.nextUrl.pathname) 
+  return response;
+
+//     let response = NextResponse.next({
+//       request: {
+//         headers: request.headers,
+//       },
+//     });
+
+//     const supabase = createServerClient(
+//       process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//       process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+//       {
+//         cookies: {
+//           getAll() {
+//             return request.cookies.getAll();
+//           },
+//           setAll(cookiesToSet) {
+//             cookiesToSet.forEach(({ name, value }) =>
+//               request.cookies.set(name, value),
+//             );
+//             // response = NextResponse.next({
+//             //   request,
+//             // });
+//             cookiesToSet.forEach(({ name, value, options }) =>
+//               response.cookies.set(name, value, options),
+//             );
+//           },
+//         },
+//       },
+//     );
  
-const user = await supabase.auth.getUser(); 
-const path = new URL(request.url).pathname;   
-const unprotectedPaths = ["/sign-in/", "/forgot-password/"]; 
-if (request.nextUrl.pathname.startsWith("/protected/") && user.error) {
-return NextResponse.redirect(new URL("/sign-in/", request.url));
-}
+// const user = await supabase.auth.getUser(); 
+// const path = new URL(request.url).pathname;   
+// const unprotectedPaths = ["/sign-in/", "/forgot-password/"]; 
+// if (request.nextUrl.pathname.startsWith("/protected/") && user.error) {
+// return NextResponse.redirect(new URL("/sign-in/", request.url));
+// }
  
-   const isUnprotectedPath = unprotectedPaths.some((up) => path.startsWith(up));
-  if (user.data.user && isUnprotectedPath) { 
-      return NextResponse.redirect(new URL("/", request.url));
-     } 
+//    const isUnprotectedPath = unprotectedPaths.some((up) => path.startsWith(up));
+//   if (user.data.user && isUnprotectedPath) { 
+//       return NextResponse.redirect(new URL("/", request.url));
+//      } 
  
-     // return response;
-     response.headers.set('confirm',request.nextUrl.searchParams.toString() ) 
-     response.headers.set('x-url',request.nextUrl.pathname) 
-    return NextResponse.next({
-      request: {
-        headers: request.headers,
-      },
+//      // return response;
+//      response.headers.set('confirm',request.nextUrl.searchParams.toString() ) 
+//      response.headers.set('x-url',request.nextUrl.pathname) 
+//     return NextResponse.next({
+//       request: {
+//         headers: request.headers,
+//       },
     
-    });
+//     });
 
 };
+ 
