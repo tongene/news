008d5770bae29e23cfa6efbcdf488b40f938731e
tongene <ngene.christina@gmail.com>
2025-11-25 09:23:41 +0000

    deploy-1/25/11

--- app/api/newsletter-cron/route.ts
+++ app/api/newsletter-cron/route.ts
@@ -52,7 +52,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
             <h2 style="color:#2c3e50;">Today's Top Stories</h2> 
            <div style="max-width: 600px; margin: auto; font-family: Arial, sans-serif; background-color: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0 0 10px rgba(0,0,0,0.05);">
            
-         ${p.image ? `<img src="${p.image}" alt="Newsletter Banner" style="width: 50%; border-radius: 6px; margin-bottom: 20px;" />`: `<img src='https://culturays.com/opengraph-image.png' alt="Newsletter Banner" style="width: 50%; border-radius: 6px; margin-bottom: 20px;" /> `} 
+         ${p.image ? `<img src="${p.image}" alt="Newsletter Banner" style="width: 100%; border-radius: 6px; margin-bottom: 20px;" />`: `<img src='https://culturays.com/opengraph-image.png' alt="Newsletter Banner" style="width: 100%; border-radius: 6px; margin-bottom: 20px;" /> `} 
 
         <h2 style="font-size: 22px; color: #2c3e50; margin: 10px 0;">${p.title}</h2>
      ${p.url ? `<p><a href="${p.url}/" style="display:inline-block;margin-top:1em;padding:0.5em 1em;background:#0070f3;color:white;text-decoration:none;border-radius:5px;">Read Full Post</a></p>` : ""}
@@ -82,7 +82,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
        <hr style="margin: 40px 0; border: none; border-top: 1px solid #eaeaea;" />        
        <h2 style="font-size: 22px; color: #2c3e50; margin: 10px 0;">News Made for You</h2>
 
-        <img src="https://culturays.com/tinitasks-poster.JPG" alt="Tini Tasks Poster" style="width: 25%; border-radius: 6px; margin-bottom: 20px;" />
+        <img src="https://culturays.com/tinitasks-poster.JPG/" alt="Tini Tasks Poster" style="width: 25%; border-radius: 6px; margin-bottom: 20px;" />
 
         <p><a href="https://gowork.africareinvented.com/" style="display:inline-block;margin-top:1em;padding:0.5em 1em;background:#0070f3;color:white;text-decoration:none;border-radius:5px;">Connect. Collaborate. Conquer on Tini Tasks</a></p>
 
