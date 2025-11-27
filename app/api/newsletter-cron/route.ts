2343b86b154cdeb63466ce664583a8b1f37639ae
tongene <ngene.christina@gmail.com>
2025-11-27 09:30:09 +0000

    deploy-1/27/11

--- app/api/newsletter-cron/route.ts
+++ app/api/newsletter-cron/route.ts
@@ -33,7 +33,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
   
     const { data: subscribers, error: subError } = await supabase
       .from("newsletter")
-      .select("email, name")
+      .select("email, name", { count: 'exact' })
       .eq("unsubscribed", false);
 
     if (subError || !subscribers || subscribers.length === 0) {
@@ -49,10 +49,10 @@ const resend = new Resend(process.env.RESEND_API_KEY);
         .filter((p) => p.title)
         .map(
           (p) => `
-            <h2 style="color:#2c3e50;">Today's Top Stories</h2> 
+           
            <div style="max-width: 600px; margin: auto; font-family: Arial, sans-serif; background-color: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0 0 10px rgba(0,0,0,0.05);">
            
-         ${p.image ? `<img src="${p.image}" alt="Newsletter Banner" style="width: 100%; border-radius: 6px; margin-bottom: 20px;" />`: `<img src='https://culturays.com/opengraph-image.png' alt="Newsletter Banner" style="width: 100%; border-radius: 6px; margin-bottom: 20px;" /> `} 
+         <img src='https://culturays.com/opengraph-image.png' alt="Newsletter Banner" style="width: 100%; border-radius: 6px; margin-bottom: 20px;" /> 
 
         <h2 style="font-size: 22px; color: #2c3e50; margin: 10px 0;">${p.title}</h2>
      ${p.url ? `<p><a href="${p.url}/" style="display:inline-block;margin-top:1em;padding:0.5em 1em;background:#0070f3;color:white;text-decoration:none;border-radius:5px;">Read Full Post</a></p>` : ""}
@@ -71,6 +71,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
         .join("");
  
       const htmlContent = `
+       <h2 style="color:#2c3e50;">Today's Top Stories</h2> 
     <div style="max-width: 600px; margin: auto; font-family: Arial, sans-serif; background-color: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0 0 10px rgba(0,0,0,0.05);">
         
        <p style="font-size: 35px; color: #2c3e50; margin: 10px 0; font-weight: bold;">Have you seen our AI Aid?<a href="https://culturays.com/naija-events/" style="display:inline-block;margin-top:1em;padding:0.5em 1em;background:#0070f3;color:white;text-decoration:none;border-radius:5px;">Try it!</a></p>
@@ -82,7 +83,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
        <hr style="margin: 40px 0; border: none; border-top: 1px solid #eaeaea;" />        
        <h2 style="font-size: 22px; color: #2c3e50; margin: 10px 0;">News Made for You</h2>
 
-        <img src="https://culturays.com/tinitasks-poster.JPG/" alt="Tini Tasks Poster" style="width: 25%; border-radius: 6px; margin-bottom: 20px;" />
+        <img src="https://culturays.com/tinitasks-poster.JPG/" alt="Tini Tasks Poster" style="width: 50%; border-radius: 6px; margin-bottom: 20px;" />
 
         <p><a href="https://gowork.africareinvented.com/" style="display:inline-block;margin-top:1em;padding:0.5em 1em;background:#0070f3;color:white;text-decoration:none;border-radius:5px;">Connect. Collaborate. Conquer on Tini Tasks</a></p>
 
