9143a75c194f6f28fcf67a29bf934b07374180ac
tongene <ngene.christina@gmail.com>
2026-03-17 18:46:56 +0000

    deploy-17.1/03/26

--- app/api/newsletter-cron/route.ts
+++ app/api/newsletter-cron/route.ts
@@ -53,8 +53,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
            <div style="max-width: 600px; margin: auto; font-family: Arial, sans-serif; background-color: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0 0 10px rgba(0,0,0,0.05);">
            ${ p.video? `<a href="${p.video}" style="position:relative; display:inline-block;">
   <img src="https://culturays.com/opengraph-image.png" alt="Watch video" />
-  <img src=">>"
-       style="position:absolute; top:50%; left:50%; transform:translate(-50%, -50%);" />
+  <p style="width: 200px; height: 200px; position:absolute; top:50%; left:50%; transform:translate(-50%, -50%);">>></p>
 </a>`:""}
         ${p.image? `<img src=${p.image} alt="Newsletter Banner" style="width: 100%; border-radius: 6px; margin-bottom: 20px;" />`: `<img src='https://culturays.com/opengraph-image.png' alt="Newsletter Banner" style="width: 100%; border-radius: 6px; margin-bottom: 20px;" />`}
 
